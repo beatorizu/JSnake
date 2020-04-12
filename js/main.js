b3c11b21014a98f4ff27d6d586b1c1e05d5925a7
beatorizu <betrizaf@gmail.com>
2020-04-12 17:48:24 -0300

    feat: Add snake

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
 let canvas = document.getElementById("board");
 let ctx = canvas.getContext("2d");
 let box = 32;
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
 
 function createBackground() {
     ctx.fillStyle = "lightgreen";
     ctx.fillRect(0, 0, 16 * box, 16 * box);
 }
 
+function createSnake() {
+    for (let i = 0; i < snake.length; i++) {
+        ctx.fillStyle = "green";
+        ctx.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
 createBackground();
+createSnake();
