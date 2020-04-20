07bc5216e9c8163ff17f598a12d15e400e4f3821
beatorizu <betrizaf@gmail.com>
2020-04-20 14:40:54 -0300

    refactor: Naming things properly to make code legible

--- js/constants.js
+++ js/constants.js
@@ -5,3 +5,10 @@ const KEY = {
     UP: [87, 38]
 };
 Object.freeze(KEY);
+
+const BOX = 32;
+
+const LEFT_WALL = 0;
+const RIGHT_WALL = 15 * BOX;
+const BOTTOM_WALL = 15 * BOX;
+const TOP_WALL = 0;
--- js/main.js
+++ js/main.js
@@ -1,32 +1,31 @@
 let canvas = document.getElementById("board");
 let ctx = canvas.getContext("2d");
-let box = 32;
 let snake = [];
 snake[0] = {
-    x: 8 * box,
-    y: 8 * box
+    x: 8 * BOX,
+    y: 8 * BOX
 }
 let direction = 'right';
 let food = {
-    x: Math.floor(Math.random() * 15 + 1) * box,
-    y: Math.floor(Math.random() * 15 + 1) * box
+    x: Math.floor(Math.random() * 15 + 1) * BOX,
+    y: Math.floor(Math.random() * 15 + 1) * BOX
 }
 
 function createBackground() {
     ctx.fillStyle = "lightgreen";
-    ctx.fillRect(0, 0, 16 * box, 16 * box);
+    ctx.fillRect(0, 0, 16 * BOX, 16 * BOX);
 }
 
 function createSnake() {
     for (let i = 0; i < snake.length; i++) {
         ctx.fillStyle = "green";
-        ctx.fillRect(snake[i].x, snake[i].y, box, box);
+        ctx.fillRect(snake[i].x, snake[i].y, BOX, BOX);
     }
 }
 
 function createFood() {
     ctx.fillStyle = "purple";
-    ctx.fillRect(food.x, food.y, box, box);
+    ctx.fillRect(food.x, food.y, BOX, BOX);
 }
 
 document.addEventListener('keydown', update);
@@ -39,10 +38,10 @@ function update(event) {
 }
 
 function gameLoop() {
-    if (snake[0].x > 15 * box) snake[0].x = 0;
-    if (snake[0].x < 0) snake[0].x = 15 * box;
-    if (snake[0].y > 15 * box) snake[0].y = 0;
-    if (snake[0].y < 0) snake[0].y = 15 * box;
+    if (snake[0].x > RIGHT_WALL) snake[0].x = LEFT_WALL;
+    if (snake[0].x < LEFT_WALL) snake[0].x = RIGHT_WALL;
+    if (snake[0].y > BOTTOM_WALL) snake[0].y = TOP_WALL;
+    if (snake[0].y < TOP_WALL) snake[0].y = BOTTOM_WALL;
 
     for (let i = 1; i < snake.length; i++) {
         if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
@@ -58,16 +57,16 @@ function gameLoop() {
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
 
-    if (direction == 'right') snakeX += box;
-    if (direction == 'left') snakeX -= box;
-    if (direction == 'down') snakeY += box;
-    if (direction == 'up') snakeY -= box;
+    if (direction == 'right') snakeX += BOX;
+    if (direction == 'left') snakeX -= BOX;
+    if (direction == 'down') snakeY += BOX;
+    if (direction == 'up') snakeY -= BOX;
 
     if (snakeX != food.x || snakeY != food.y) {
         snake.pop();
     } else {
-        food.x = Math.floor(Math.random() * 15 + 1) * box;
-        food.y = Math.floor(Math.random() * 15 + 1) * box;
+        food.x = Math.floor(Math.random() * 15 + 1) * BOX;
+        food.y = Math.floor(Math.random() * 15 + 1) * BOX;
     }
 
     let newHead = {
